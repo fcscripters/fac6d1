e0c927950dfefae037a722eeefad6bb1dee2cd7d
tormod17 <tormodsmith@gmail.com>
2015-09-14 23:29:28 +0100

    updated radio buttons and added bmi function

--- app1.js
+++ app1.js
@@ -2,98 +2,55 @@
   console.log("hello world");
 
 
-  var fullnameInput = document.getElementById('fullname');
-
-
-  var ageInput =  document.getElementById('age');
-  var weightInput =  document.getElementById('weight');
+  var fullnameInput =document.getElementById('fullname');
+  var ageInput =    document.getElementById('age');
+  var weightInput = document.getElementById('weight');
   var heightInput = document.getElementById('height');
-  //requires radio button
-//  var exerciseInput = document.getElementById('exercise');
+  ///radio buttons value ///
+  var radios =  document.getElementsByName('exercise');
 
-  //requires dropdown
-  var smokeInput = document.getElementById('smoke');
-  var emailInput = document.getElementById('email');
+   // smoke drop down menu
+  var smokeInput =  document.getElementById('smoke');
+  // input box
+  var emailInput =  document.getElementById('email');
   //checkbox
   var optinInput = document.getElementById('optin');
+  // input box
+  var test = document.getElementById('health-form');
+  // bmi function
+  function bmi(weight,height){
+    var bmi= weight/height*height;
+    alert("You have a Body Mass Index of"+bmi);
+  };
 
-var test = document.getElementById('health-form');
-
-console.log(test);
 
   test.addEventListener('submit', function(e) {
   e.preventDefault();
+      var exerciseInput='';
+      for (var i = 0; i <=radios.length; i++) {
+        console.log(radios[i].value);
+        console.log(radios[i].checked);
+
+        if (radios[i].type ==='radio' && radios[i].checked) {
+            // do whatever you want with the checked radio
+            exerciseInput =radios[i].value;
+            console.log(exerciseInput);
+             // only one radio can be logically checked, don't check the rest
+            break;
+        }
+      }
   console.log("{Full Name: "+fullnameInput.value+"}");
   console.log("{Age: "+ageInput.value+"}");
   console.log("{Weight: "+weightInput.value+"}");
   console.log("{Height: "+heightInput.value+"}");
+  console.log("{Exercise:"+exerciseInput+"}");
   console.log("{Smoke: "+smokeInput.value+"}");
   console.log("{Email: "+emailInput.value+"}");
-//  console.log("{Exercise: "+exerciseInput.value+"}");
-  //console.log("{Optin: "+optinInput.value+"}");
+  //prompt bmi ///
+  bmi(weightInput.value,heightInput.value);
 
 });
 
 
 
-var radios = document.getElementsByName('exercise');
-console.log(radios);
 
-
-
-for (var i = 0; i<radios.length; i++) {
-    console.log(radios[i].value);
-    console.log(radios[i].checked);
-
-    if (radios[i].type === 'radio' && radios[i].checked) {
-        // do whatever you want with the checked radio
-        var testing =radios[i].value;
-        console.log(testing);
-
-        // only one radio can be logically checked, don't check the rest
-        break;
-    }
-}
-
-/*
-
-var exerciseoutput = (getRadioValue("exercise",exerciseInput)+1);
-
- function getRadioValue(groupName, groupVal) {
-            var valRet = 0;
-            var radios = document.getElementsByName(groupName).value;
-            for (var i = 0; i < radios.length; i++) {
-        if (radios[i].checked) {
-            groupVal = i;
-            break;
-        }
-
-    }
-    // needs changed to log individual radio button title ?
-    return groupVal;
-  };
-
-  /*
-
-function getDropValue(groupName, groupVal) {
-            var valRet = 0;
-            var drops = document.getElementsByName(groupName);
-            for (var i = 0; i < drops.length; i++) {
-        if (drops[i].checked) {
-            groupVal = i;
-            break;
-        }
-
-    }
-    // needs changed to log individual radio button title ?
-    return groupVal;
-  };
-
-
-
-
-  getRadioValue("exercise", exerciseInput)+1);
-
-
-
-  */
